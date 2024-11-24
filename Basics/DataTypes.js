89c4a2eab0673c476e9273a95c6fe94ce8eb8d55
Muhammad Danial <notdanial22@gmail.com>
2024-11-24 20:38:06 +0500

    Initial commit

---
+++ Basics/DataTypes.js
@@ -0,0 +1,26 @@
+
+// number 
+// bigint => for more accuracy check mostly used for web3
+// string
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+
+let myNumber = 10;
+let myBigInt = 10n;
+let myString = "Hello";
+let myBoolean = true;
+let myNull = null;
+let Undefined;
+
+
+console.log(myNumber, myBigInt, myString, myBoolean , myNull, Undefined)
+
+console.log(typeof null); // null returns object
+console.log(typeof undefined); // undefined returns undefined
+
+// in my opinion null returning an object is necessary because how would we no if there was nothing or was it undefined or it was empty . Well , it's just my opinion.
+
