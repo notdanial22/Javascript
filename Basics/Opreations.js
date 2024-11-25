88618a96dbbf4394f7d4c655f3b2166c1736c0f8
Muhammad Danial <notdanial22@gmail.com>
2024-11-25 12:50:56 +0500

    Operations

---
+++ Basics/Opreations.js
@@ -0,0 +1,52 @@
+const number = 10
+
+let negativeNumber = -number
+
+// console.log(negativeNumber) // Output: -10
+
+// Simple Operations:
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**3); // 8;  acts as a power of 2
+// console.log(2/2); // 1
+// console.log(2%3); // 2; returns the remainder
+
+// adding strings
+
+const strA = 'hello'
+const strB = 'world'
+
+// console.log(strA + strB) // Output: "helloworld"
+
+// we cannot subtract two strings
+
+// following are some examples of the operations we should not do at any cost cause it's not school and a good code is also easy to read so no need to do these complex calculations:
+
+console.log('1' + 2) //12
+console.log(1 + '2') //12
+console.log('1' + 2 + 2) // 122
+console.log(1 + 2 + '2') // 32
+
+// in my opinion it just starts from left to right if not parentheses while adding string to numbers so in first it goes like this "1" + 2 = "12" + 2 = "122"
+// same in second case 1 + 2 = 3 + "2" = 32
+
+// console.log(1 + (2 + 2));   //
+
+let candies = 5
+
+// Prefix
+console.log(++candies) // Shows 6 (adds 1 first, then shows)
+console.log(candies) // Shows 6
+
+// Postfix
+let toys = 5
+console.log(toys++) // Shows 5 (shows first, then adds 1)
+console.log(toys) // Shows 6
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2-2 // why do this is there a need to do such complex things??
+
+
