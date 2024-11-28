aaa437d514334dd22423e1548f95f892dfa1204b
Muhammad Danial <notdanial22@gmail.com>
2024-11-28 18:24:21 +0500

    String

---
+++ Basics/String.js
@@ -0,0 +1,61 @@
+const str1 = "hello world."
+
+const str2 = "My name is Danial     "
+
+const str3 = "5"
+
+const str4 = "Three cats were behind the wall. One of the cats had white fur."
+
+
+// Concatenation using the + operator
+
+const concatenatedStr = str1 + str2
+
+console.log(concatenatedStr)
+
+// Using the template literals (backticks)
+
+console.log(str1.charAt(6));
+
+console.log(str1.length);
+
+console.log(str1.indexOf("d"));
+
+console.log(str1.includes("w"));
+
+console.log(str1.startsWith("hello"));
+
+
+console.log(str1.endsWith("."));
+
+console.log(str1.toUpperCase());
+
+console.log(str1.toLowerCase());
+
+console.log(str2.trim());
+
+console.log(str2.replace("Danial", "John"));
+
+console.log(str3.padStart(5, "4"));
+
+console.log(str3.padEnd(5, "0"));
+
+console.log(str2.slice(0,6));
+
+console.log(str1.split(""));
+
+console.log(str4.replaceAll('cats', "dogs"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
