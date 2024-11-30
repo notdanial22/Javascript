b7e0f31d4743fe5667ad029376c577363f11022e
Muhammad Danial <notdanial22@gmail.com>
2024-11-30 17:18:20 +0500

    Array

---
+++ Basics/Array.js
@@ -0,0 +1,65 @@
+// Array 
+
+//  array from iterable objects like strings
+const StrArr = Array.from('hello');
+console.log(StrArr) // ['h', 'e', 'l', 'l', 'o']
+
+// an array from arguments provided
+const arr1 = Array.of(1, 2, 3);
+console.log(arr1); // [1, 2, 3]
+
+const arr3 = [1, 2]
+arr3.push(3) // Adds 3
+console.log(arr3) // [1, 2, 3]
+arr3.pop() // Removes 3
+console.log(arr3)
+
+
+const arr4 = [2, 3, 5, 7,4]
+arr4.unshift(1); // Adds 1
+console.log(arr4);
+arr4.shift(); // Removes 1
+console.log(arr4);
+
+// mapping function creates a new array and applying function to each element
+const squaredArray = arr4.map(x => x**2)
+
+console.log(squaredArray);
+
+// filtering function creates a new array with elements that pass the test implemented by provided function
+const filteredArray = arr4.filter(x => x % 2 === 0 )
+
+console.log(filteredArray);
+
+// reduce function applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value
+
+const sum = arr4.reduce((acc, curr) => acc + curr, 0)
+
+console.log(sum)
+
+// find 
+
+console.log(arr4.find(x => x / 7 == 1 ));
+
+
+console.log(arr4.includes(2))
+
+console.log(arr1.concat(arr4))
+
+
+console.log(arr4.slice(1, 3)) // [3, 5]
+
+console.log(arr4.sort((a, b) => a - b)) // [ 2, 3, 4, 5]
+
+arr4.splice(0, 1, 'a', 'b') // Removes 2 elements starting at index 0 and inserts 'a', 'b'
+console.log(arr4)
+
+// what is main difference between slice and splice ??
+// slice mainly creates  a modified copy of the original array without changing the original
+// 1. start (optional): The index to begin extraction (inclusive). 2. end (optional): The index to stop extraction (exclusive).
+
+// splice Removes, replaces, or adds elements to an array by modifying the original array.
+
+//start: The index to begin changes.
+//deleteCount (optional): The number of elements to remove.
+//...items (optional): Elements to insert.
